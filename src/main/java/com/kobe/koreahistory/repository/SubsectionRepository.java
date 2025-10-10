28467c6f1f0b0c1506b4e312ffff2abb386ec039
Kobe <dev.skyachieve91@gmail.com>
2025-10-11 06:34:36 +0900

    feat: Subsection 리포지토리 추가
    - SubsectionRepository 인터페이스 생성
    - JpaRepository 상속으로 기본 CRUD 기능 제공

---
+++ src/main/java/com/kobe/koreahistory/repository/SubsectionRepository.java
@@ -0,0 +1,18 @@
+package com.kobe.koreahistory.repository;
+
+import com.kobe.koreahistory.domain.entity.Subsection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * packageName    : com.kobe.koreahistory.repository
+ * fileName       : SubsectionRepository
+ * author         : kobe
+ * date           : 2025. 10. 10.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025. 10. 10.        kobe       최초 생성
+ */
+public interface SubsectionRepository extends JpaRepository<Subsection, Long> {
+}
