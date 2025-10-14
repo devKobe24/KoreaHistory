ae589d09b560b64202485a95a9307847718312cf
Kobe <dev.skyachieve91@gmail.com>
2025-10-15 08:49:45 +0900

    feat: 키워드 컬렉션 검색 기능 추가
    - findByKeywordsContaining: keywords 컬렉션에서 특정 문자열 포함 검색
    - @Query와 JOIN을 사용하여 @ElementCollection 테이블 조회
    - 부분 일치 검색(LIKE) 지원

--- src/main/java/com/kobe/koreahistory/repository/KeywordRepository.java
+++ src/main/java/com/kobe/koreahistory/repository/KeywordRepository.java
@@ -2,6 +2,11 @@ package com.kobe.koreahistory.repository;
 
 import com.kobe.koreahistory.domain.entity.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * packageName    : com.kobe.koreahistory.repository
@@ -15,4 +20,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * 2025. 10. 7.        kobe       최초 생성
  */
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+	@Query("SELECT k FROM Keyword k JOIN k.keywords keywordValue WHERE keywordValue LIKE %:keyword%")
+	Optional<List<Keyword>> findByKeywordsContaining(@Param("keyword") String keyword);
+
+	Optional<Keyword> findById(Long id);
 }
