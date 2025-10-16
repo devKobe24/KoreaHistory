4e5c174a24b8bcac41382c1f895e30ddbac5f5f0
Kobe <dev.skyachieve91@gmail.com>
2025-10-16 09:25:04 +0900

    feat: 섹션 제목 수정 응답 DTO 추가
    - PatchSectionTitleResponseDto 클래스 신규 생성
    - 섹션 엔티티 기반 응답 데이터 변환 로직 구현
    - id, sectionNumber, sectionTitle, subsections 필드를 포함한 응답 DTO 구현
    - 하위 섹션 목록을 CreateSubsectionResponseDto로 변환하여 반환

---
+++ src/main/java/com/kobe/koreahistory/dto/response/section/PatchSectionTitleResponseDto.java
@@ -0,0 +1,36 @@
+package com.kobe.koreahistory.dto.response.section;
+
+import com.kobe.koreahistory.domain.entity.Section;
+import com.kobe.koreahistory.dto.response.subsection.CreateSubsectionResponseDto;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * packageName    : com.kobe.koreahistory.dto.response.section
+ * fileName       : PatchSectionTitleResponseDto
+ * author         : kobe
+ * date           : 2025. 10. 16.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025. 10. 16.        kobe       최초 생성
+ */
+@Getter
+public class PatchSectionTitleResponseDto {
+	private final Long id;
+	private final Integer sectionNumber;
+	private final String sectionTitle;
+	private final List<CreateSubsectionResponseDto> subsections;
+
+	public PatchSectionTitleResponseDto(Section entity) {
+		this.id = entity.getId();
+		this.sectionNumber = entity.getSectionNumber();
+		this.sectionTitle = entity.getSectionTitle();
+		this.subsections = entity.getSubsections().stream()
+			.map(CreateSubsectionResponseDto::new)
+			.collect(Collectors.toList());
+	}
+}
