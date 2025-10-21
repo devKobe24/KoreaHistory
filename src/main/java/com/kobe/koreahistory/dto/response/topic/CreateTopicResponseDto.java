942eb3894b2f9db2fbc254a66e6d7c32028661d7
Kobe <dev.skyachieve91@gmail.com>
2025-10-22 02:50:22 +0900

    refactor: CreateTopicResponseDto 응답 최적화
    생성 응답에서 하위 엔티티 상세 대신 상위 참조 ID 반환
    - keywordGroup 제거 (Keyword 리스트 DTO 변환 로직 제거)
    - subsectionId 추가 (부모 Subsection 참조)
    - Stream API를 통한 변환 로직 제거

--- src/main/java/com/kobe/koreahistory/dto/response/topic/CreateTopicResponseDto.java
+++ src/main/java/com/kobe/koreahistory/dto/response/topic/CreateTopicResponseDto.java
@@ -1,11 +1,7 @@
 package com.kobe.koreahistory.dto.response.topic;
 
 import com.kobe.koreahistory.domain.entity.Topic;
-import com.kobe.koreahistory.dto.response.keyword.CreateKeywordResponseDto;
 import lombok.Getter;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * packageName    : com.kobe.koreahistory.dto.response.topic
@@ -17,24 +13,19 @@ import java.util.stream.Collectors;
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2025. 10. 12.        kobe       최초 생성
+ * 2025. 10. 21.        kobe       수정
  */
 @Getter
 public class CreateTopicResponseDto {
 	private final Long id;
 	private final Integer topicNumber;
 	private final String topicTitle;
-	private final List<CreateKeywordResponseDto> keywordGroup;
+	private final Long subsectionId;
 
-	// Entity를 인자로 받는 단일 public 생성자로 통일
 	public CreateTopicResponseDto(Topic entity) {
 		this.id = entity.getId();
 		this.topicNumber = entity.getTopicNumber();
 		this.topicTitle = entity.getTopicTitle();
-
-		// Topic에 포함된 Keyword들을 DTO로 변환하는 로직
-		this.keywordGroup = entity.getKeywords().stream()
-			.map(CreateKeywordResponseDto::new)
-			.collect(Collectors.toList());
-
+		this.subsectionId = entity.getSubsection().getId();
 	}
 }
