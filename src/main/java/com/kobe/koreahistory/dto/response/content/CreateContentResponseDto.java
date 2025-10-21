f78184f7af658ea03a5190d6154171fcce7ff3fd
Kobe <dev.skyachieve91@gmail.com>
2025-10-22 02:46:10 +0900

    refactor: CreateContentResponseDto 응답 필드 변경
    details 대신 keywordId를 반환하도록 수정
    - details (List<String>) 필드 제거
    - keywordId (Long) 필드 추가
    - 상위 엔티티 참조 정보 제공으로 변경

--- src/main/java/com/kobe/koreahistory/dto/response/content/CreateContentResponseDto.java
+++ src/main/java/com/kobe/koreahistory/dto/response/content/CreateContentResponseDto.java
@@ -3,8 +3,6 @@ package com.kobe.koreahistory.dto.response.content;
 import com.kobe.koreahistory.domain.entity.Content;
 import lombok.Getter;
 
-import java.util.List;
-
 /**
  * packageName    : com.kobe.koreahistory.dto.response.content
  * fileName       : CreateContentResponseDto
@@ -15,14 +13,15 @@ import java.util.List;
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2025. 10. 14.        kobe       최초 생성
+ * 2025. 10. 21.        kobe       수정
  */
 @Getter
 public class CreateContentResponseDto {
 	private final Long id;
-	private final List<String> details;
+	private final Long keywordId;
 
 	public CreateContentResponseDto(Content entity) {
 		this.id = entity.getId();
-		this.details = entity.getDetails();
+		this.keywordId = entity.getKeyword().getId();
 	}
 }
