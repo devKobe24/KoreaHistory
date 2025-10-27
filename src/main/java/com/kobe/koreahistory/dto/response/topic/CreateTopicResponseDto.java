8777fe4143258c2db93c41ef96527e477bfda562
devKobe24 <dev.skyachieve91@gmail.com>
2025-10-27 12:39:39 +0900

    feat(topic): CreateTopicResponseDto에 하위 Keyword 목록 추가
    Topic 생성 응답에 하위 Keyword 정보 포함
    - keywords 필드 추가 (List<CreateKeywordResponseDto>)
    - Topic 엔티티의 keywords를 DTO로 변환하여 포함
    - Stream API 활용한 변환 로직 구현

--- src/main/java/com/kobe/koreahistory/dto/response/topic/CreateTopicResponseDto.java
+++ src/main/java/com/kobe/koreahistory/dto/response/topic/CreateTopicResponseDto.java
@@ -1,7 +1,11 @@
 package com.kobe.koreahistory.dto.response.topic;
 
 import com.kobe.koreahistory.domain.entity.Topic;
+import com.kobe.koreahistory.dto.response.keyword.CreateKeywordResponseDto;
 import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * packageName    : com.kobe.koreahistory.dto.response.topic
@@ -21,11 +25,15 @@ public class CreateTopicResponseDto {
 	private final Integer topicNumber;
 	private final String topicTitle;
 	private final Long subsectionId;
+	private final List<CreateKeywordResponseDto> keywords;
 
 	public CreateTopicResponseDto(Topic entity) {
 		this.id = entity.getId();
 		this.topicNumber = entity.getTopicNumber();
 		this.topicTitle = entity.getTopicTitle();
 		this.subsectionId = entity.getSubsection().getId();
+		this.keywords = entity.getKeywords().stream()
+			.map(CreateKeywordResponseDto::new)
+			.collect(Collectors.toList());
 	}
 }
